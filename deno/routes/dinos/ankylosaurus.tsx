7f7ed3588ae916b8284e7b54b6a1493133f5cc5d
Daniel Schramm <daniel.schramm@emvi.com>
2023-02-21 22:26:13 +0100

    Favicon and image attribution.

--- deno/routes/dinos/ankylosaurus.tsx
+++ deno/routes/dinos/ankylosaurus.tsx
@@ -45,6 +45,7 @@ export default function Ankylosaurus({data}: PageProps<AnkylosaurusData>) {
                 <p>Ankylosaurus is a genus of armored dinosaur. Its fossils have been found in geological formations dating to the very end of the Cretaceous Period, about 68–66 million years ago, in western North America, making it among the last of the non-avian dinosaurs. It was named by Barnum Brown in 1908; it is monotypic, containing only A. magniventris. The generic name means "fused" or "bent lizard", and the specific name means "great belly".</p>
                 <p>Ankylosaurus was the largest-known ankylosaurine dinosaur and possibly the largest ankylosaurid. In 2004 Carpenter estimated that the individual with the largest-known skull, which is 64.5cm long and 74.5cm wide, was about 6.25m long and had a hip height of about 1.7m.</p>
                 <img src="ankylosaurus.png" alt="Ankylosaurus" style="width: 100%;" />
+                <span style="display: block; text-align: center;">emilywilloughby.com (CC BY-SA 3.0)</span>
                 <OrderDino variant={orderVariant} dino="Ankylosaurus" success={success} error={error} />
             </main>
             <Footer />
