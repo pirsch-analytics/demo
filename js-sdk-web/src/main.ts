5309f64ce6ce3278c1604a000338d89a83633b5b
Marvin Blum <marvin.blum@emvi.com>
2023-10-03 17:45:29 +0200

    Small improvements to SDK web demo.

--- js-sdk-web/src/main.ts
+++ js-sdk-web/src/main.ts
@@ -4,15 +4,17 @@ async function main() {
     try {
         // Create the client using the identification code.
         const client = new Pirsch({
+            identificationCode: "",
+
+            // Only required for proxies and testing.
             //baseUrl: "https://localhost.com:9999",
-            identificationCode: ""
         });
 
         // Send a page view.
         await client.hit();
 
         // Send an event.
-        await client.event("Custom Event 2", 0, {
+        await client.event("Custom Event", 0, {
             meta_data: "field"
         });
     } catch (error) {
--- js-sdk-web/vite.config.js
+++ js-sdk-web/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     preview: {
-        //host: "localhost.com",
+        host: "localhost.com",
         port: 8081,
     },
 });
